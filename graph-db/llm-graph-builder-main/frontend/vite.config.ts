ef69e8192351e102ed24ebe9459bb095661e70a3
Bash-G <bashar.geran@outlook.com>
2025-07-05 10:23:56 +0200

    Frontend changes to support React 18 and Vite

--- graph-db/llm-graph-builder-main/frontend/vite.config.ts
+++ graph-db/llm-graph-builder-main/frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
 // see https://stackoverflow.com/questions/73834404/react-uncaught-referenceerror-process-is-not-defined
@@ -9,7 +10,14 @@ export default defineConfig(({ mode }) => {
     define: {
       'process.env': env,
     },
-    plugins: [react()],
-    optimizeDeps: { esbuildOptions: { target: 'es2020' } },
-  };
-});
+    plugins: [tailwindcss(),
+      react()],
+    optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2020'
+    }
+  },
+  css: {
+    devSourcemap: true
+  }
+}});
