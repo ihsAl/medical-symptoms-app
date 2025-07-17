c7db28e1ab210fbf53253a4e05b22ecbad8898e2
ihsAl <83977238+ihsAl@users.noreply.github.com>
2025-07-17 22:24:48 +0200

    Use model mistral:instruct instead of mistral for performance reasons

--- llm/llmService.js
+++ llm/llmService.js
@@ -4,10 +4,12 @@ const axios = require('axios');
 
 const OLLAMA_API = 'http://localhost:11434/api/generate';
 
+
+// use model mistral-instruct instead of mistral because it perfoms better in dialog
 async function askLLM(prompt) {
     try {
         const response = await axios.post(OLLAMA_API, {
-            model: 'mistral',
+            model: 'mistral:instruct',
             prompt: prompt,
             stream: false
         });
